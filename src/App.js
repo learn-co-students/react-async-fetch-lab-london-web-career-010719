c4912ab2c0ff62e8e27b15f13d1a89162a6bd2f7
studioshane <studioshane@gmail.com>
2019-03-18 09:56:09 +0000

    fetch working, not rendering

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React from "react";
+
+const URL = "http://api.open-notify.org/astros.json";
+
+export default class App extends React.Component {
+  state = {
+    peopleInSpace: []
+  };
+
+  componentWillMount() {
+    this.getAstronauts();
+    this.showAstronauts();
+  }
+
+  getAstronauts = () => {
+    fetch(URL)
+      .then(resp => resp.json())
+      .then(astronauts => this.setState({ peopleInSpace: astronauts }));
+  };
+
+  showAstronauts = () => {};
+
+  render() {
+    return (
+      <ul>
+        <li>{this.showAstronauts()}</li>
+      </ul>
+    );
+  }
+}
